1289a18d93484bd55a71916d75df93412013b62f
ateistemes <timur.tynychbekov@gmail.com>
2023-10-18 14:22:49 +0600

    m

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,15 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int sign = 1;
-        if( multiplyByAndToInclusive < 0){
-            sign = -1;
-            multiplyByAndToInclusive *= -1;
-        }
-        int k = 0;
-        while( k < multiplyByAndToInclusive){
-            System.out.println(k);
-            k*=multiplyByAndToInclusive;
+        int positiveCount = 0;
+        int negativeCount = 0;
+        if (multiplyByAndToInclusive < 0) {
+            while (negativeCount >= multiplyByAndToInclusive) {
+                System.out.println(positiveCount * multiplyByAndToInclusive);
+                positiveCount++;
+                negativeCount--;
+            }
+        } else if (multiplyByAndToInclusive > 0) {
+            while (multiplyByAndToInclusive >= positiveCount) {
+                System.out.println(positiveCount * multiplyByAndToInclusive);
+                positiveCount++;
+            }
+        } else {
+            System.out.print("");
         }
     }
 }
